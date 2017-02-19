338266af9cd4cbf68d8ca5d35d1d0c8dff6add0c
Nevinson <nevinson23@gmail.com>
2017-02-19 20:22:14 +0200

    Entity Models
    Add database entity models.

---
+++ Nevinson.Libary.Data/Models/Video.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Nevinson.Libary.Data.Models
+{
+    public class Video : LibraryAsset
+    {
+        [Required]
+        public string Director { get; set; }
+    }
+}
