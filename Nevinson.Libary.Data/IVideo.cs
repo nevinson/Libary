48b54f1c9b7ab981f984b63cce2bb2766495f39c
Nevinson <nevinson23@gmail.com>
2017-02-21 00:08:52 +0200

    Video Service
    Add video service interface and logic.

---
+++ Nevinson.Libary.Data/IVideo.cs
@@ -0,0 +1,13 @@
+﻿using Nevinson.Libary.Data.Models;
+using System.Collections.Generic;
+
+namespace Nevinson.Libary.Data
+{
+    public interface IVideo
+    {
+        IEnumerable<Video> GetAll();
+        IEnumerable<Video> GetByDirector(string author);
+        Video Get(int id);
+        void Add(Video newVideo);
+    }
+}
