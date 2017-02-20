b4d2642817028656b14b5c54987c72be507abb93
Nevinson <nevinson23@gmail.com>
2017-02-20 22:25:45 +0200

    Book Service
    Add book service interface and logic.

---
+++ Nevinson.Libary.Data/IBook.cs
@@ -0,0 +1,14 @@
+﻿using Nevinson.Libary.Data.Models;
+using System.Collections.Generic;
+
+namespace Nevinson.Libary.Data
+{
+    public interface IBook
+    {
+        IEnumerable<Book> GetAll();
+        IEnumerable<Book> GetByAuthor(string author);
+        IEnumerable<Book> GetByISBN(string isbn);
+        Book Get(int id);
+        void Add(Book newBook);
+    }
+}
