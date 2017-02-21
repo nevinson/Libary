9bc5919fb1c4ac3cdd4c3f14dd88a67ce33a2082
Nevinson <nevinson23@gmail.com>
2017-02-21 23:34:26 +0200

    Library Card Service
    Add library card service interface and logic.

---
+++ Nevinson.Libary.Data/ILibraryCard.cs
@@ -0,0 +1,12 @@
+﻿using Nevinson.Libary.Data.Models;
+using System.Collections.Generic;
+
+namespace Nevinson.Libary.Data
+{
+    public interface ILibraryCard
+    {
+        IEnumerable<LibraryCard> GetAll();
+        LibraryCard Get(int id);
+        void Add(LibraryCard newCard);
+    }
+}
---
+++ Nevinson.Libary.Service/LibraryCardService.cs
@@ -0,0 +1,33 @@
+﻿using Nevinson.Libary.Data;
+using Nevinson.Libary.Data.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Nevinson.Libary.Service
+{
+    public class LibraryCardService : ILibraryCard
+    {
+        private LibraryDbContext _context; // private field to store the context.
+
+        public LibraryCardService(LibraryDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(LibraryCard newLibraryCard)
+        {
+            _context.Add(newLibraryCard);
+            _context.SaveChanges();
+        }
+
+        public LibraryCard Get(int cardId)
+        {
+            return _context.LibraryCards.FirstOrDefault(p => p.Id == cardId);
+        }
+
+        IEnumerable<LibraryCard> ILibraryCard.GetAll()
+        {
+            return _context.LibraryCards;
+        }
+    }
+}
