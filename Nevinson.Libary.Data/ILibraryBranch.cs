44946e67213d0b5a943ee356c26d8a6b945f69c6
Nevinson <nevinson23@gmail.com>
2017-02-21 21:58:40 +0200

    Library Branch Service
    Add library branch service interface and logic.

---
+++ Nevinson.Libary.Data/ILibraryBranch.cs
@@ -0,0 +1,19 @@
+﻿using Nevinson.Libary.Data.Models;
+using System.Collections.Generic;
+
+namespace Nevinson.Libary.Data
+{
+    public interface ILibraryBranch
+    {
+        IEnumerable<LibraryBranch> GetAll();
+        IEnumerable<Patron> GetPatrons(int branchId);
+        IEnumerable<LibraryAsset> GetAssets(int branchId);
+        LibraryBranch Get(int branchId);
+        void Add(LibraryBranch newBranch);
+        IEnumerable<string> GetBranchHours(int branchId);
+        bool IsBranchOpen(int branchId);
+        int GetAssetCount(IEnumerable<LibraryAsset> libraryAssets);
+        int GetPatronCount(IEnumerable<Patron> patrons);
+        decimal GetAssetsValue(int id);
+    }
+}
