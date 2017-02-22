7f38b433e4004bd465c9c22683177bff35fd02c1
Nevinson <nevinson23@gmail.com>
2017-02-22 03:59:00 +0200

    Patron Service
    Add patron service interface and logic.

---
+++ Nevinson.Libary.Data/IPatron.cs
@@ -0,0 +1,15 @@
+﻿using Nevinson.Libary.Data.Models;
+using System.Collections.Generic;
+
+namespace Nevinson.Libary.Data
+{
+    public interface IPatron
+    {
+        IEnumerable<Patron> GetAll();
+        Patron Get(int id);
+        void Add(Patron newBook);
+        IEnumerable<CheckoutHistory> GetCheckoutHistory(int patronId);
+        IEnumerable<Hold> GetHolds(int patronId);
+        IEnumerable<Checkout> GetCheckouts(int id);
+    }
+}
