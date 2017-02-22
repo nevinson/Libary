686836eebae93bc3135383ba89f257c00910856a
Nevinson <nevinson23@gmail.com>
2017-02-22 22:25:47 +0200

    Status Service
    Add status service interface and logic.

---
+++ Nevinson.Libary.Data/IStatus.cs
@@ -0,0 +1,12 @@
+﻿using Nevinson.Libary.Data.Models;
+using System.Collections.Generic;
+
+namespace Nevinson.Libary.Data
+{
+    public interface IStatus
+    {
+        IEnumerable<Status> GetAll();
+        Status Get(int id);
+        void Add(Status newStatus);
+    }
+}
---
+++ Nevinson.Libary.Service/StatusService.cs
@@ -0,0 +1,33 @@
+﻿using Nevinson.Libary.Data;
+using Nevinson.Libary.Data.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Nevinson.Libary.Service
+{
+    public class StatusService : IStatus
+    {
+        private LibraryDbContext _context; // private field to store the context.
+
+        public StatusService(LibraryDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(Status newStatus)
+        {
+            _context.Add(newStatus);
+            _context.SaveChanges();
+        }
+
+        public Status Get(int id)
+        {
+            return _context.Statuses.FirstOrDefault(p => p.Id == id);
+        }
+
+        public IEnumerable<Status> GetAll()
+        {
+            return _context.Statuses;
+        }
+    }
+}
