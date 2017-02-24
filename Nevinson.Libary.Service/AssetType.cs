0fe5246b7a7154fbd9bcf82ff7a8bd3bcd9d7cee
Nevinson <nevinson23@gmail.com>
2017-02-24 03:48:52 +0200

    Asset Type
    Add asset type service interface.

---
+++ Nevinson.Libary.Service/AssetType.cs
@@ -0,0 +1,12 @@
+﻿using Nevinson.Libary.Data;
+using Nevinson.Libary.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nevinson.Libary.Service
+{
+    public class AssetType// : IAssetType
+    {
+    }
+}
