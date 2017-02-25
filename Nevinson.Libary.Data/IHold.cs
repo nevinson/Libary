6509b1ae3b5ce602af78eb89772030d4a3dc5f7d
Nevinson <nevinson23@gmail.com>
2017-02-26 01:36:55 +0200

    Book Hold
    Add book hold interface.

---
+++ Nevinson.Libary.Data/IHold.cs
@@ -0,0 +1,12 @@
+﻿using Nevinson.Libary.Data.Models;
+using System.Collections.Generic;
+
+namespace Nevinson.Libary.Data
+{
+    public interface IHold
+    {
+        IEnumerable<AssetType> GetAll();
+        AssetType Get(int id);
+        void Add(AssetType newType);
+    }
+}
