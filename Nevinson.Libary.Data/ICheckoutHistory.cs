d4a11324d55a62f97a7e13c2681edfb274a56c4f
Nevinson <nevinson23@gmail.com>
2017-02-26 01:11:10 +0200

    Checkout History
    Add checkout history interface.

---
+++ Nevinson.Libary.Data/ICheckoutHistory.cs
@@ -0,0 +1,14 @@
+﻿using Nevinson.Libary.Data.Models;
+using System.Collections.Generic;
+
+namespace Nevinson.Libary.Data
+{
+    public interface ICheckoutHistory
+    {
+        IEnumerable<CheckoutHistory> GetAll();
+        IEnumerable<CheckoutHistory> GetForAsset(LibraryAsset asset);
+        IEnumerable<CheckoutHistory> GetForCard(LibraryCard card);
+        CheckoutHistory Get(int id);
+        void Add(CheckoutHistory newCheckoutHistory);
+    }
+}
