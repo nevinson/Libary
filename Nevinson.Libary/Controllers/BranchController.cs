0c2a385495b92f7d21252373bcc20d1cefd0768d
Nevinson <nevinson23@gmail.com>
2017-03-01 22:18:13 +0200

    Library Branch
    Add library branch controller.

---
+++ Nevinson.Libary/Controllers/BranchController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nevinson.Libary.Data;
+using Nevinson.Libary.Models.Branch;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nevinson.Libary.Controllers
+{
+    public class BranchController : Controller
+    {
+        private ILibraryBranch _branch;
+
+        // create a constructor takes branchservice
+        public BranchController(ILibraryBranch branch)
+        {
+            // save branchService param off into a private field 
+            // to have access in the rest of the controller
+            _branch = branch;
+        }
+
+        public IActionResult Index()
+        {
+            var branchModels = _branch.GetAll()
+                .Select(br => new BranchDetailModel
+                {
+                    Id = br.Id,
+                    BranchName = br.Name,
+                    NumberOfAssets = _branch.GetAssetCount(br.LibraryAssets),
+                    NumberOfPatrons = _branch.GetPatronCount(br.Patrons),
+                    IsOpen = _branch.IsBranchOpen(br.Id)
+                }).ToList();
+
+            var model = new BranchIndexModel()
+            {
+                Branches = branchModels
+            };
+
+            return View(model);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            var branch = _branch.Get(id);
+            var model = new BranchDetailModel
+            {
+                BranchName = branch.Name,
+                Description = branch.Description,
+                Address = branch.Address,
+                Telephone = branch.Telephone,
+                BranchOpenedDate = branch.OpenDate.ToString("yyyy-MM-dd"),
+                NumberOfPatrons = _branch.GetPatronCount(branch.Patrons),
+                NumberOfAssets = _branch.GetAssetCount(branch.LibraryAssets),
+                TotalAssetValue = _branch.GetAssetsValue(id),
+                ImageUrl = branch.ImageUrl,
+                HoursOpen = _branch.GetBranchHours(id)
+            };
+
+            return View(model);
+        }
+    }
+}
