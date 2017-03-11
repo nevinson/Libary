854fea0b3d89f0bf58f34d2717a357d34b18c355
Nevinson <nevinson23@gmail.com>
2017-03-11 23:35:42 +0200

    Catalog
    Add catalog models.

---
+++ Nevinson.Libary/Models/Catalog/AssetCheckinModel.cs
@@ -0,0 +1,14 @@
+﻿using Nevinson.Libary.Data.Models;
+using System;
+
+namespace Nevinson.Libary.Models.Catalog
+{
+    public class AssetCheckinModel
+    {
+        public LibraryCard LibraryCard { get; set; }
+        public DateTime DateDue { get; set; }
+        public DateTime DateCheckedOut { get; set; }
+        public decimal FeesCharged { get; set; }
+        public int DaysOverDue { get; set; }
+    }
+}
---
+++ Nevinson.Libary/Models/Catalog/AssetCheckoutModel.cs
@@ -0,0 +1,14 @@
+﻿using Nevinson.Libary.Data.Models;
+using System;
+
+namespace Nevinson.Libary.Models.Catalog
+{
+    public class AssetCheckoutModel
+    {
+        public string Title { get; set; }
+        public string ImageUrl { get; set; }
+        public LibraryCard LibraryCard { get; set; }
+        public DateTime DateDue { get; set; }
+        public DateTime DateCheckedOut { get; set; }
+    }
+}
